d9e4cc137a5dfda8b04efa3427846a98ff025cba
Hammad Ali <49644704+hammad11ali@users.noreply.github.com>
2019-12-20 12:23:39 +0500

    removed unneccessory files

--- check.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-col_names =  ['A', 'B', 'C']
-my_df  = pd.DataFrame(columns = col_names)
-my_df.loc[len(my_df)] = [2, 4, 5]
-my_df.loc[len(my_df)] = [92, 54, 36]
-my_df.loc[len(my_df)] = [27, 47, 35]
-my_df.to_csv("result.csv",index=False)
-
